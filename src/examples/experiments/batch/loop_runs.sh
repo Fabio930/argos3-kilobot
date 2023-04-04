6725c6b98caf467507b795fa572a6534234dddc3
Fabio Oddi <fabio.oddi@uniroma1.it>
2023-04-04 17:09:04 +0200

    .

--- src/examples/experiments/batch/loop_runs.sh
+++ src/examples/experiments/batch/loop_runs.sh
@@ -33,12 +33,12 @@ experiment_length="3601"
 RUNS=100
 numrobots="20"
 kappa="0.75"
-branches="2 4"
+branches="2"
 control_param="1 3"
 
 for nrob in $numrobots; do
     dir=$res_dir/"Robots#"$nrob
-    depth="2"
+    depth="1"
     if [[ ! -e $dir ]]; then
         mkdir $dir
     fi
