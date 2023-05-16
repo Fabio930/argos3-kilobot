ed6dc5b5d669385c33489f6dcef4eb14d9fcdd16
Fabio Oddi <fabio.oddi@uniroma1.it>
2023-05-16 16:04:55 +0200

    .

--- src/examples/behaviors/quorum_structure.h
+++ src/examples/behaviors/quorum_structure.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 uint32_t expiring_ticks_quorum = 10000;
-uint8_t min_quorum_length = 12;
+uint8_t min_quorum_length = 11;
 uint8_t min_quorum_items = 3;
 float quorum_scaling_factor = .8;
 uint8_t num_quorum_items = 0;
