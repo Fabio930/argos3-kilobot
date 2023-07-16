7c60aef359b6391a2d3d93be51bc70fa9482c76a
Fabio Oddi <fabio.oddi@uniroma1.it>
2023-07-16 12:24:45 +0200

    refactored batch testing

--- src/examples/experiments/batch/loop_runs.sh
+++ src/examples/experiments/batch/loop_runs.sh
@@ -29,57 +29,59 @@ echo "$CONFIGURATION_FILE" | egrep "^$SHARED_DIR" &> /dev/null || exit 1
 #######################################
 ### experiment_length is in seconds ###
 #######################################
-experiment_length="7201"
-RUNS=100
+experiment_length="1801"
+RUNS=2
 numrobots="20"
-kappa="0.75"
-branches="2"
-depth="2 4"
-control_param="1 3"
+rebroadcast="0 1"
+minimum_quorum_length="10"
+quorum_scaling_factor=".6 .7"
+committed_percentage=".6 .7"
 
-for nrob in $numrobots; do
-    dir=$res_dir/"Robots#"$nrob
+strToReplace="."
+replace="_"
+
+for par0 in $rebroadcast; do
+    dir=$res_dir/"Rebroadcast#"$par0
     if [[ ! -e $dir ]]; then
         mkdir $dir
     fi
-    for par1 in $branches; do
-        dir1=$dir/"Branches#"$par1
+    for par1 in $numrobots; do
+        dir1=$dir/"Robots#"$par1
         if [[ ! -e $dir1 ]]; then
             mkdir $dir1
         fi
-        for par2 in $depth; do
-            dir2=$dir1/"Depth#"$par2
+        for par2 in $committed_percentage; do
+            par2BIS=${par2//$strToReplace/$replace}
+            dir2=$dir1/"CommitPerc#"$par2BIS
             if [[ ! -e $dir2 ]]; then
                 mkdir $dir2
             fi
-            for par3 in $kappa; do
-                strToReplace="."
-                replace="_"
-                par3BIS=${par3//$strToReplace/$replace}
-                dir3=$dir2/"K#"$par3BIS
+            for par3 in $minimum_quorum_length; do
+                dir3=$dir2/"Qlen#"$par3
                 if [[ ! -e $dir3 ]]; then
                     mkdir $dir3
                 fi
-                for par4 in $control_param; do
-                    dir4=$dir3/"R#"$par4"_S#"$experiment_length
+                for par4 in $quorum_scaling_factor; do
+                    par4BIS=${par4//$strToReplace/$replace}
+                    dir4=$dir3/"Scaling#"$par4BIS"_S#"$experiment_length
                     if [[ ! -e $dir4 ]]; then
                         mkdir $dir4
                     fi
                     for it in $(seq 1 $RUNS); do
-                        config=`printf 'config_nrob%d_branches%d_depth%d_K%d_R%d_run%d.argos' $nrob $par1 $par2 $par3 $par4 $it`
+                        config=`printf 'config_rebroad%d_nrobots%d_CommitPerc%d_qLen%d_scalingFact%d_run%d.argos' $par0 $par1 $par2 $par3 $par4 $it`
                         echo config $config
                         cp $base_config $config
-                        sed -i "s|__NUMROBOTS__|$nrob|g" $config
-                        sed -i "s|__BRANCHES__|$par1|g" $config
-                        sed -i "s|__DEPTH__|$par2|g" $config
-                        sed -i "s|__K__|$par3|g" $config
-                        sed -i "s|__R__|$par4|g" $config
+                        sed -i "s|__BROADCAST_POLICY__|$par0|g" $config
+                        sed -i "s|__NUMROBOTS__|$par1|g" $config
+                        sed -i "s|__COMMITTED_PERCENTAGE__|$par2|g" $config
+                        sed -i "s|__MIN_QUORUM_LEN__|$par3|g" $config
+                        sed -i "s|__QUORUM_SCALING_FACTOR__|$par4|g" $config
                         sed -i "s|__SEED__|$it|g" $config
                         sed -i "s|__TIMEEXPERIMENT__|$experiment_length|g" $config
                         dt=$(date '+%d-%m-%Y_%H-%M-%S')
                         kilo_file="${dt}__run#${it}_LOG.tsv"
                         sed -i "s|__KILOLOG__|$kilo_file|g" $config
-                        echo "Running next configuration Robots $nrob Branches $par1 Depth $par2 K $par3 R $par4 File $kilo_file"
+                        echo "Running next configuration Rebroadcast $par0 Robots $par1 CommittedPercentage $par2 MinQuorumLen $par3 QuorumScalingFactor $par4 File $kilo_file"
                         echo "argos3 -c $1$config"
                         argos3 -c './'$config
                         mv $kilo_file $dir4
