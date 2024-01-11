8ef6abfc157dec429c102b6ea2dcfcb91b001b5d
Fabio Oddi <fabio.oddi@uniroma1.it>
2024-01-11 11:45:29 +0100

    .

--- print_results.py
+++ print_results.py
@@ -1,5 +1,4 @@
-# usage python3 light_print_results.py -d data -f files -t ticks
-# if -d is declared then must specify which data to print: all, quorum or freq(uency)
+# usage python3 light_print_results.py -f files -t ticks
 # if -f is declared then must specify which files to print: all, first, last or rand(om)
 # if -t is declared then must specify which is the log frequency, default value is 31
 import bestNresults as BNres
@@ -7,19 +6,13 @@ import os, sys
 
 def check_inputs():
     ticks = 10
-    data_type = "all"
     files_to_elaborate = "all"
-    if len(sys.argv)>7:
+    if len(sys.argv)>5:
         print("Too many arguments --EXIT--")
         exit()
     if len(sys.argv)>1:
         for i in range(len(sys.argv)):
-            if sys.argv[i]=='-d':
-                if i+1 >= len(sys.argv):
-                    print("BAD format input --EXIT--")
-                    exit()
-                data_type = str(sys.argv[i+1])
-            elif sys.argv[i]=='-f':
+            if sys.argv[i]=='-f':
                 if i+1 >= len(sys.argv):
                     print("BAD format input --EXIT--")
                     exit()
@@ -33,20 +26,17 @@ def check_inputs():
                 except:
                     print("BAD format input\n-t must be followed by a positve integer --EXIT--")
                     exit()
-    if data_type!="all" and data_type!="quorum" and data_type!="freq":
-        print("BAD format -d input type\nallowed entries are: all, quorum or freq --EXIT--")
-        exit()
     if files_to_elaborate!="all" and files_to_elaborate!="first" and files_to_elaborate!="last" and files_to_elaborate!="rand":
         print("BAD format -f input type\nallowed entries are: all, first, last or rand --EXIT--")
         exit()
     if ticks <= 0:
         print("BAD format -t input type\nmust input a positive integer greater than zero --EXIT--")
         exit()
-    return ticks,data_type,files_to_elaborate
+    return ticks,files_to_elaborate
 
 def main():
     bestNresults = BNres.Results()
-    bestNresults.ticks_per_sec, data_type, files_to_elaborate = check_inputs()
+    bestNresults.ticks_per_sec, files_to_elaborate = check_inputs()
     for base in bestNresults.bases:
         for adir in sorted(os.listdir(base)):
             if '.' not in adir and '#' in adir:
@@ -65,7 +55,7 @@ def main():
                                         n_agents=int(zzdir.split('#')[1])
                                         ddtemp=os.path.join(dtemp, zzdir)
                                         print("\nOpening folder",ddtemp)
-                                if(data_type == "all" or data_type == "quorum"): bestNresults.extract_k_quorum_data(ddtemp,exp_length,n_agents,files_to_elaborate)
+                                        bestNresults.extract_k_quorum_data(ddtemp,exp_length,n_agents,files_to_elaborate)
 
 if __name__ == "__main__":
     main()
