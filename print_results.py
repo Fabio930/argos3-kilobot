775b817a201007ec32d30d142fca58a7a25fd6b7
Fabio Oddi <fabio.oddi@uniroma1.it>
2024-02-06 10:45:54 +0100

    .

--- print_results.py
+++ print_results.py
@@ -38,19 +38,29 @@ def main():
     bestNresults = BNres.Results()
     bestNresults.ticks_per_sec, files_to_elaborate = check_inputs()
     max_buff_dim = 0
+    max_exp_time = 0
+    max_n_agents = 0
+    max_exp_len = 0
     for base in bestNresults.bases:
         for adir in sorted(os.listdir(base)):
             if '.' not in adir and '#' in adir:
-                exp_length=int(adir.split('#')[1])
                 pre_path=os.path.join(base, adir)
-                for zdir in sorted(os.listdir(pre_path)):
-                    if '.' not in zdir and '#' in zdir:
-                        dtemp=os.path.join(pre_path, zdir)
-                        for zzdir in sorted(os.listdir(dtemp)):
-                            if '.' not in zzdir and '#' in zzdir:
-                                ddtemp=os.path.join(dtemp, zzdir)
-                                tmp = bestNresults.check_data_dim(ddtemp,exp_length)
-                                if tmp > max_buff_dim: max_buff_dim = tmp
+                exp_length=int(adir.split('#')[1])
+                if exp_length >= max_exp_len:
+                    max_exp_len = exp_length
+                    
+                    for zdir in sorted(os.listdir(pre_path)):
+                        if '.' not in zdir and '#' in zdir:
+                            dtemp=os.path.join(pre_path, zdir)
+                            for zzdir in sorted(os.listdir(dtemp)):
+                                if '.' not in zzdir and '#' in zzdir:
+                                    n_agents=int(zdir.split('#')[1])
+                                    if n_agents >= max_n_agents:
+                                        max_n_agents = n_agents
+                                        ddtemp=os.path.join(dtemp, zzdir)
+                                        tmp_b,tmp_m = bestNresults.check_data_dim(ddtemp,exp_length,max_exp_time)
+                                        if tmp_b > max_buff_dim: max_buff_dim = tmp_b
+                                        if tmp_m > max_exp_time: max_exp_time = tmp_m
     for base in bestNresults.bases:
         for adir in sorted(os.listdir(base)):
             if '.' not in adir and '#' in adir:
