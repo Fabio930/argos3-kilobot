7eba6bad4c17d6f1221bd9c616c39dc594ea47de
Fabio Oddi <fabio.oddi@uniroma1.it>
2024-02-21 12:33:53 +0100

    .

--- plot_results.py
+++ plot_results.py
@@ -1,20 +1,28 @@
-# usage python3 light_print_results.py -f files -t ticks
-# if -f is declared then must specify which files to print: all, first, last or rand(om)
-# if -t is declared then must specify which is the log frequency, default value is 31
 import csv_results as CSVres
-import os, sys
+import os
 
+##################################################################################
 def main():
     csv_res = CSVres.Data()
+    limit = 0.8
     for base in csv_res.bases:
-        for adir in sorted(os.listdir(base)):
-            pre_path=os.path.join(base, adir)
-            if 'proc_data' in adir:
-                for file in sorted(os.listdir(pre_path)):
-                    n_runs=file.split('.')[0].split('#')[-1]
-                    csv_path=os.path.join(pre_path, file)
-                    data = csv_res.read_csv(csv_path,n_runs)
-                    csv_res.print_hetmap(data)               
+        for file in sorted(os.listdir(base)):
+            if file != "images":
+                file_path=os.path.join(base, file)
+                no_ext_file = file.split('.')[0]
+                sets = no_ext_file.split('_')
+                algo = sets[0][0]
+                for s in sets:
+                    val = s.split('#')
+                    if len(val)>1:
+                        if val[0]=='r':
+                            n_runs=val[1]
+                        elif val[0]=='a':
+                            arena=val[1]
+                data = csv_res.read_csv(file_path,algo,n_runs,arena)
+                keys, states, buffers, times, messages_counts = csv_res.divide_data(data)               
+                csv_res.plot_heatmaps(keys,(states,times,buffers),limit)
 
+##################################################################################
 if __name__ == "__main__":
     main()
