d3fb72ebc668fff99a9152bc6671846d9d55cc59
Fabio Oddi <fabio.oddi@uniroma1.it>
2024-03-27 14:50:35 +0100

    .

--- csv_results.py
+++ csv_results.py
@@ -111,7 +111,7 @@ class Data:
         return (algorithm, arena_size, n_runs, exp_time, communication, n_agents, gt, thrlds, min_buff_dim, msg_time), states, times, buffer, (messages_b, messages_r)
     
 ##########################################################################################################
-    def plot_active(self,data_in,times):
+    def plot_active(self,data_in,times,msg_buffer):
         if not os.path.exists(self.base+"/proc_data/c_images/"):
             os.mkdir(self.base+"/proc_data/c_images/")
         path = self.base+"/proc_data/c_images/"
@@ -162,6 +162,7 @@ class Data:
                                             for thr in threshlds:
                                                 s_data = data_in[i].get((a,a_s,n_r,et,c,n_a,str(gt),str(thr),m_b_d,m_t))
                                                 t_data = times[i].get((a,a_s,n_r,et,c,n_a,str(gt),str(thr),m_b_d,m_t))
+                                                b_data = msg_buffer[i].get((a,a_s,n_r,et,c,n_a,str(gt),str(thr),m_b_d,m_t))
                                                 if s_data != None:
                                                     if ((i==2 or i==3) and m_t not in p_k) or ((i==0 or i==1) and m_t not in o_k):
                                                         p_k.append(m_t) if (i==2 or i==3) else o_k.append(m_t)
@@ -212,7 +213,13 @@ class Data:
         self.print_borders(path,'avg','min',ground_T,threshlds,[dict_park_avg,dict_adms_avg,dict_our_avg],[dict_park_tmin,dict_adms_tmin,dict_our_tmin],[p_k,o_k],[arena,agents])
         self.print_borders(path,'max','max',ground_T,threshlds,[dict_park_max,dict_adms_max,dict_our_max],[dict_park_tmax,dict_adms_tmax,dict_our_tmax],[p_k,o_k],[arena,agents])
         self.print_borders(path,'reg','median',ground_T,threshlds,[dict_park_fin,dict_adms_fin,dict_our_fin],[dict_park_tmed,dict_adms_tmed,dict_our_tmed],[p_k,o_k],[arena,agents])
+        # self.print_messages(path,'msgs')
+##########################################################################################################
+    def print_messages(self,path,_type,data_in,keys):
+        plt.rcParams.update({"font.size":22})
 
+        return
+    
 ##########################################################################################################
     def print_borders(self,path,_type,t_type,ground_T,threshlds,data_in,times_in,keys,more_k):
         plt.rcParams.update({"font.size":22})
@@ -232,18 +239,22 @@ class Data:
         tvalsa = [[0]*len(threshlds)]*len(o_k)
         tvalso = [[0]*len(threshlds)]*len(o_k)
 
-        dots        = mlines.Line2D([], [], color='black', marker='o', linestyle='None', markeredgewidth=6, markersize=6, label='P = 0.2')
-        triangles   = mlines.Line2D([], [], color='black', marker='^', linestyle='None', markeredgewidth=6, markersize=6, label='P = 0.8')
-        red         = mlines.Line2D([], [], color='r', marker='_', linestyle='None', markeredgewidth=12, markersize=12, label='Anonymous')
-        blue        = mlines.Line2D([], [], color='b', marker='_', linestyle='None', markeredgewidth=12, markersize=12, label='ID+B')
-        green       = mlines.Line2D([], [], color='g', marker='_', linestyle='None', markeredgewidth=12, markersize=12, label='ID+R')
+        dots        = mlines.Line2D([], [], color='black', marker='None', linestyle='--', linewidth=4, label='P = 0.2')
+        triangles   = mlines.Line2D([], [], color='black', marker='None', linestyle='-', linewidth=4, label='P = 0.8')
+        red         = mlines.Line2D([], [], color='r', marker='_', linestyle='None', markeredgewidth=18, markersize=18, label='Anonymous')
+        blue        = mlines.Line2D([], [], color='b', marker='_', linestyle='None', markeredgewidth=18, markersize=18, label='ID+B')
+        green       = mlines.Line2D([], [], color='g', marker='_', linestyle='None', markeredgewidth=18, markersize=18, label='ID+R')
         void        = mlines.Line2D([], [], linestyle='None')
 
-        handles_c   = [triangles,dots,void]
+        handles_c   = [triangles,dots]
         handles_r   = [red,blue,green]
         fig, ax     = plt.subplots(nrows=3, ncols=4,figsize=(28,24))
         tfig, tax   = plt.subplots(nrows=3, ncols=4,figsize=(28,24))
         str_threshlds = []
+        void_str_threshlds = []
+        svoid_str_threshlds = []
+        void_str_gt = []
+        void_str_tim = []
         for a in arena:
             if a=="smallA":
                 agents = ["25"]
@@ -363,44 +374,82 @@ class Data:
                         tvalsp[k][th] = p_valst
                         tvalsa[k][th] = a_valst
                         tvalso[k][th] = o_valst
-                    ax[row][k].plot(vals2p[k],color=colors_map[0],marker='o')
-                    ax[row][k].plot(vals8p[k],color=colors_map[0],marker='^')
-                    ax[row][k].plot(vals2a[k],color=colors_map[1],marker='o')
-                    ax[row][k].plot(vals8a[k],color=colors_map[1],marker='^')
-                    ax[row][k].plot(vals2o[k],color=colors_map[2],marker='o')
-                    ax[row][k].plot(vals8o[k],color=colors_map[2],marker='^')
-                    tax[row][k].plot(tvalsp[k],color=colors_map[0])
-                    tax[row][k].plot(tvalsa[k],color=colors_map[1])
-                    tax[row][k].plot(tvalso[k],color=colors_map[2])
+                    ax[row][k].plot(vals2p[k],color=colors_map[0],lw=6,ls='--')
+                    ax[row][k].plot(vals8p[k],color=colors_map[0],lw=6,ls='-')
+                    ax[row][k].plot(vals2a[k],color=colors_map[1],lw=6,ls='--')
+                    ax[row][k].plot(vals8a[k],color=colors_map[1],lw=6,ls='-')
+                    ax[row][k].plot(vals2o[k],color=colors_map[2],lw=6,ls='--')
+                    ax[row][k].plot(vals8o[k],color=colors_map[2],lw=6,ls='-')
+                    ax[row][k].plot(np.arange(0.5,1.01,0.01),color='black',lw=3,ls=':')
+                    tax[row][k].plot(tvalsp[k],color=colors_map[0],lw=6)
+                    tax[row][k].plot(tvalsa[k],color=colors_map[1],lw=6)
+                    tax[row][k].plot(tvalso[k],color=colors_map[2],lw=6)
                     if len(str_threshlds)==0:
                         for x in threshlds:
                             if np.round(np.round(x,1)-np.round(x%10,2),2) == 0.0:
                                 str_threshlds.append(str(x))
+                                void_str_threshlds.append('')
+                                svoid_str_threshlds.append('')
                             else:
-                                str_threshlds.append('')
+                                void_str_threshlds.append('')
+                        for x in range(5,11,1):
+                            void_str_gt.append('')
+                        for x in range(0,601,100):
+                            void_str_tim.append('')
                     if row==2:
-                        ax[row][k].set_xticks(np.arange(len(str_threshlds)),labels=str_threshlds)
-                        tax[row][k].set_xticks(np.arange(len(str_threshlds)),labels=str_threshlds)
+                        ax[row][k].set_xticks(np.arange(0,51,10),labels=str_threshlds)
+                        tax[row][k].set_xticks(np.arange(0,51,10),labels=str_threshlds)
+                        ax[row][k].set_xticks(np.arange(0,51,1),labels=void_str_threshlds,minor=True)
+                        tax[row][k].set_xticks(np.arange(0,51,1),labels=void_str_threshlds,minor=True)
+                        if k==0:
+                            ax[row][k].set_xlabel("T_m60")
+                            tax[row][k].set_xlabel("T_m60")
+                        elif k==1:
+                            ax[row][k].set_xlabel("T_m120")
+                            tax[row][k].set_xlabel("T_m120")
+                        elif k==2:
+                            ax[row][k].set_xlabel("T_m300")
+                            tax[row][k].set_xlabel("T_m300")
+                        elif k==3:
+                            ax[row][k].set_xlabel("T_m600")
+                            tax[row][k].set_xlabel("T_m600")
                     else:
-                        ax[row][k].get_xaxis().set_visible(False)
-                        tax[row][k].get_xaxis().set_visible(False)
+                        ax[row][k].set_xticks(np.arange(0,51,10),labels=svoid_str_threshlds)
+                        tax[row][k].set_xticks(np.arange(0,51,10),labels=svoid_str_threshlds)
+                        ax[row][k].set_xticks(np.arange(0,51,1),labels=void_str_threshlds,minor=True)
+                        tax[row][k].set_xticks(np.arange(0,51,1),labels=void_str_threshlds,minor=True)
                     if k==0:
                         ax[row][k].set_yticks(np.arange(.5,1.01,.1))
-                        tax[row][k].set_yticks(np.arange(0,901,50))
+                        tax[row][k].set_yticks(np.arange(0,601,100))
+                        ax[row][k].set_yticks(np.arange(.5,1.01,.01),labels=void_str_threshlds,minor=True)
+                        tax[row][k].set_yticks(np.arange(0,601,25),labels=['' for x in range(0,601,25)],minor=True)
+                        if row==0:
+                            ax[row][k].set_ylabel("LD25")
+                            tax[row][k].set_ylabel("LD25")
+                        elif row==1:
+                            ax[row][k].set_ylabel("HD25")
+                            tax[row][k].set_ylabel("HD25")
+                        elif row==2:
+                            ax[row][k].set_ylabel("HD100")
+                            tax[row][k].set_ylabel("HD100")
                     else:
-                        ax[row][k].get_yaxis().set_visible(False)
-                        tax[row][k].get_yaxis().set_visible(False)
-        fig.supxlabel('Threshold')
-        fig.supylabel('Ground Truth')
-        tfig.supxlabel('Threshold')
-        tfig.supylabel('Simulation Secs')
+                        ax[row][k].set_yticks(np.arange(.5,1.01,.1),labels=void_str_gt)
+                        tax[row][k].set_yticks(np.arange(0,601,100),labels=void_str_tim)
+                        ax[row][k].set_yticks(np.arange(.5,1.01,.01),labels=void_str_threshlds,minor=True)
+                        tax[row][k].set_yticks(np.arange(0,601,25),labels=['' for x in range(0,601,25)],minor=True)
+                    ax[row][k].grid(which='major')
+                    tax[row][k].grid(which='major')
+        fig.supxlabel(r"$\tau$")
+        fig.supylabel('G')
+        tfig.supxlabel(r"$\tau$")
+        tfig.supylabel('T_c (s)')
 
         fig.tight_layout()
         tfig.tight_layout()
         fig_path = path+_type+"_activation.png"
         tfig_path = path+t_type+"_time.png"
-        fig.legend(handles=handles_r+handles_c, ncol=2,loc='upper left',framealpha=.4)
-        tfig.legend(handles=handles_r+handles_c, ncol=2,loc='upper left',framealpha=.4)
+        fig.legend(handles=handles_r+handles_c,ncols=5, loc='upper right',framealpha=0.7)
+        tfig.legend(handles=handles_r,ncols=3,loc='upper right',framealpha=0.7)
         fig.savefig(fig_path)
         tfig.savefig(tfig_path)
         # plt.show()
--- plot_results.py
+++ plot_results.py
@@ -7,8 +7,9 @@ def main():
     csv_res = CSVres.Data()
     limit = 0.8
     for base in csv_res.bases:
-        tot_st = []
-        tot_times = []
+        tot_st      = []
+        tot_times   = []
+        tot_buffer  = []
         for file in sorted(os.listdir(base)):
             if "images" not in file:
                 file_path=os.path.join(base, file)
@@ -26,12 +27,14 @@ def main():
                 keys, states, times, buffers, messages_counts = csv_res.divide_data(data)     
                 # csv_res.o_plot_heatmaps(keys,(states,times,buffers),limit) if algo=='O' else csv_res.p_plot_heatmaps(keys,(states,times,buffers),limit)
                 if len(tot_st)==0:
-                    tot_st = [states]
-                    tot_times = [times]
+                    tot_st      = [states]
+                    tot_times   = [times]
+                    tot_buffer  = [buffers]
                 else:
-                    tot_st = np.append(tot_st,[states],axis=0)
-                    tot_times = np.append(tot_times,[times],axis=0)
-        csv_res.plot_active(tot_st,tot_times)
+                    tot_st      = np.append(tot_st,[states],axis=0)
+                    tot_times   = np.append(tot_times,[times],axis=0)
+                    tot_buffer  = np.append(tot_buffer,[buffers],axis=0)
+        csv_res.plot_active(tot_st,tot_times,tot_buffer)
 ##################################################################################
 if __name__ == "__main__":
     main()
