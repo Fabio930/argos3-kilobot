b0227b838053e84dd339f41d5d2ce5ea2a990979
Fabio Oddi <fabio.oddi@uniroma1.it>
2024-06-15 17:16:43 +0200

    .

--- dump_results.py
+++ dump_results.py
@@ -112,8 +112,8 @@ def main():
 
     active_processes = []
     total_memory = psutil.virtual_memory().total / (1024 * 1024)  # Total memory in MB
-    memory_per_process_25 = 1509948.44 / 1024 # Memory used by each process with 25 agents
-    memory_per_process_100 = 11085398.56 / 1024 # Memory used by each process with 100 agents
+    memory_per_process_25 = 1677721.6 / 1024 # Memory used by each process with 25 agents
+    memory_per_process_100 = 19461570.56 / 1024 # Memory used by each process with 100 agents
 
     while not queue.empty() or active_processes:
         # Calculate total memory used by active processes
