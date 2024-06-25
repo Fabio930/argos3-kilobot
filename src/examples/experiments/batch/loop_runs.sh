ce4091972d3361f3c53af5045f28730462841354
Fabio Oddi <fabioddi24@gmail.com>
2024-06-25 17:58:47 +0200

    .

--- src/examples/experiments/batch/loop_runs.sh
+++ src/examples/experiments/batch/loop_runs.sh
@@ -66,7 +66,7 @@ echo "$CONFIGURATION_FILE" | egrep "^$SHARED_DIR" &> /dev/null || exit 1
 #######################################
 ### experiment_length is in seconds ###
 #######################################
-experiment_length="1200"
+experiment_length="900"
 RUNS=100
 rebroadcast="0 2"
 msg_expiring_sec="60 120 300 600"
