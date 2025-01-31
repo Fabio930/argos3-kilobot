20b074f7b1f649b2ddedbe60b7ad83c578a32534
Fabio Oddi <fabio.oddi@uniroma1.it>
2025-01-31 10:42:32 +0100

    .

--- data_extractor.py
+++ data_extractor.py
@@ -128,20 +128,20 @@ class Results:
 ##########################################################################################################
     def extract_k_data(self,base,path_temp,max_steps,communication,n_agents,msg_exp_time,msg_hops,sub_path,data_type="all"):
         max_buff_size = n_agents - 1
-        act_results = {}
         num_runs = int(len(os.listdir(sub_path))/n_agents)
         msgs_bigM_1 = [np.array([])] * n_agents
         act_bigM_1 = [np.array([])] * n_agents
         act_bigM_2 = [np.array([])] * n_agents
         buff_neglects_bigM = [np.array([])] * n_agents
         buff_insertin_bigM = [np.array([])] * n_agents
+        buff_updates_bigM = [np.array([])] * n_agents
         msgs_M_1 = [np.array([],dtype=int)]*num_runs # x num_samples
         act_M_1 = [np.array([],dtype=int)]*num_runs
         act_M_2 = [np.array([],dtype=int)]*num_runs
         buff_neglects  = [np.array([],dtype=int)]*num_runs
         buff_insertin  = [np.array([],dtype=int)]*num_runs
-        # states_by_gt = self.assign_states(n_agents,num_runs)
-        #####################################################
+        buff_updates  = [np.array([],dtype=int)]*num_runs
+        
         for elem in sorted(os.listdir(sub_path)):
             if '.' in elem:
                 selem=elem.split('.')
@@ -153,40 +153,63 @@ class Results:
                         log_count = 0
                         for row in reader:
                             log_count += 1
+                            broadcast_c = 0
+                            re_broadcast_c = 0 
+                            buf_neglect = 0
+                            buf_insert = 0
+                            buf_update = 0
                             if log_count % self.ticks_per_sec == 0:
-                                log_count = 0
                                 msgs = []
-                                broadcast_c = 0
-                                re_broadcast_c = 0
-                                for val in row:                                            
-                                    if val.count('\t')==0:
-                                        if val!='-' : msgs.append(int(val))
-                                    else:
-                                        val = val.split('\t')
-                                        if val[0] != '': msgs.append(int(val[0]))
-                                        broadcast_c = int(val[1])
-                                        re_broadcast_c = int(val[2])
                                 if data_type in ("all","freq"):
+                                    for val in row:                                            
+                                        if val.count('\t')!=0:
+                                            val = val.split('\t')
+                                            broadcast_c = int(val[1])
+                                            re_broadcast_c = int(val[2])
+                                            buf_neglect = int(val[3])
+                                            buf_insert = int(val[4])
+                                            buf_update = int(val[5])
                                     act_M_1[seed-1] = np.append(act_M_1[seed-1],broadcast_c)
                                     act_M_2[seed-1] = np.append(act_M_2[seed-1],re_broadcast_c)
-                                if len(msgs) < max_buff_size:
-                                    for i in range(max_buff_size-len(msgs)): msgs.append(-1)
-                                if len(msgs_M_1[seed-1]) == 0:
-                                    msgs_M_1[seed-1] = [msgs]
-                                else :
-                                    msgs_M_1[seed-1] = np.append(msgs_M_1[seed-1],[msgs],axis=0)
-                    if len(msgs_M_1[seed-1])!=max_steps: print(sub_path,'\n',"run:",seed,"agent:",agent_id,"tot lines:",len(msgs_M_1[seed-1]))
+                                    buff_neglects[seed-1] = np.append(buff_neglects[seed-1],buf_neglect)
+                                    buff_insertin[seed-1] = np.append(buff_insertin[seed-1],buf_insert)
+                                    buff_updates[seed-1] = np.append(buff_updates[seed-1],buf_update)
+                                if data_type in ("all","quorum"):
+                                    for val in row:                                            
+                                        if val.count('\t')==0:
+                                            if val!='-' : msgs.append(int(val))
+                                        else:
+                                            val = val.split('\t')
+                                            if val[0] != '': msgs.append(int(val[0]))
+                                    if len(msgs) < max_buff_size:
+                                        for i in range(max_buff_size-len(msgs)): msgs.append(-1)
+                                    if len(msgs_M_1[seed-1]) == 0:
+                                        msgs_M_1[seed-1] = [msgs]
+                                    else :
+                                        msgs_M_1[seed-1] = np.append(msgs_M_1[seed-1],[msgs],axis=0)
+                    if data_type in ("all","quorum") and len(msgs_M_1[seed-1])!=max_steps:
+                        print(sub_path,'\n',"run:",seed,"agent:",agent_id,"tot lines:",len(msgs_M_1[seed-1]))
+                    elif data_type in ("freq") and len(act_M_1[seed-1])!=max_steps:
+                        print(sub_path,'\n',"run:",seed,"agent:",agent_id,"tot lines:",len(act_M_1[seed-1]))
                     if seed == num_runs:
-                        msgs_bigM_1[agent_id] = msgs_M_1
-                        msgs_M_1 = [np.array([],dtype=int)]*num_runs
                         if data_type in ("all","freq"):
                             act_bigM_1[agent_id] = act_M_1
                             act_bigM_2[agent_id] = act_M_2
                             act_M_1 = [np.array([],dtype=int)]*num_runs
                             act_M_2 = [np.array([],dtype=int)]*num_runs
+                            buff_neglects_bigM[agent_id] = buff_neglects
+                            buff_insertin_bigM[agent_id] = buff_insertin
+                            buff_updates_bigM[agent_id] = buff_updates
+                            buff_neglects = [np.array([],dtype=int)]*num_runs
+                            buff_insertin = [np.array([],dtype=int)]*num_runs
+                            buff_updates = [np.array([],dtype=int)]*num_runs
+                        if data_type in ("all","quorum"):
+                            msgs_bigM_1[agent_id] = msgs_M_1
+                            msgs_M_1 = [np.array([],dtype=int)]*num_runs
+        info_vec    = sub_path.split('/')
+        algo        = info_vec[4].split('_')[0][0]
         if data_type in ("all","quorum"):
-            info_vec     = sub_path.split('/')
-            algo     = info_vec[4].split('_')[0][0]
+            states_by_gt = self.assign_states(n_agents,num_runs)
             arenaS   = info_vec[4].split('_')[-1][:-1]
             BUFFERS = []
             if arenaS=='small':
@@ -209,7 +232,6 @@ class Results:
                             self.compute_recovery(algo,num_runs,arenaS,communication,n_agents,BUFFERS[buf],msg_hops,self.ground_truth[gt],thr,quorums,msgs_bigM_1)
                             del quorums
                         del results
-                        gc.collect()
             else:
                 messages = self.compute_meaningful_msgs(msgs_bigM_1,msg_exp_time,algo,1,1)
                 self.dump_msgs("messages_resume.csv",[arenaS,algo,communication,n_agents,msg_exp_time,msg_hops,messages])
@@ -222,12 +244,14 @@ class Results:
                         self.compute_recovery(algo,num_runs,arenaS,communication,n_agents,msg_exp_time,msg_hops,self.ground_truth[gt],thr,quorums,msgs_bigM_1)
                         del quorums
                     del results
-                    gc.collect()
+            del msgs_M_1,msgs_bigM_1,messages
         if data_type in ("all","freq"):
-            act_results[0] = (act_bigM_1,act_bigM_2)
-            self.dump_msg_freq(algo,1,act_results,len(act_M_1),base,path_temp,msg_exp_time,msg_hops)
+            act_results = (act_bigM_1,act_bigM_2)
+            self.dump_sumof(algo,1,act_results,len(act_M_1),base,path_temp,msg_exp_time,msg_hops)
+            act_results = (buff_neglects_bigM,buff_insertin_bigM,buff_updates_bigM)
+            self.dump_sumof(algo,3,act_results,len(buff_insertin),base,path_temp,msg_exp_time,msg_hops)
             del act_results
-        del num_runs,msgs_bigM_1,act_bigM_1,act_bigM_2,msgs_M_1,act_M_1,act_M_2
+        del num_runs,act_bigM_1,act_bigM_2,act_M_1,act_M_2,buff_insertin,buff_neglects,buff_updates,buff_insertin_bigM,buff_neglects_bigM,buff_updates_bigM
         gc.collect()
                 
 ##########################################################################################################
@@ -356,6 +380,12 @@ class Results:
             values.append("broadcast_msg")
         elif indx+bias==2:
             values.append("rebroadcast_msg")
+        elif indx+bias==3:
+            values.append("do_nothing_buffer")
+        elif indx+bias==4:
+            values.append("insert_buffer")
+        elif indx+bias==5:
+            values.append("update_buffer")
         values.append(data_in)
         values.append(data_std)
         fw = open(os.path.abspath("")+"/proc_data/"+file_name,mode='a',newline='\n')
@@ -366,29 +396,28 @@ class Results:
         fw.close()
 
 ##########################################################################################################
-    def dump_msg_freq(self,algo,bias,data_in,dMR,BASE,PATH,MSG_EXP_TIME,msg_hops):
-        for l in range(len(data_in.get(0))):
-            multi_run_data = data_in.get(0)[l]
-            if multi_run_data is not None:
-                flag2 = [-1]*len(multi_run_data[0][0])
-                for i in range(len([multi_run_data[0]])):
-                    flag1 = [-1]*len(multi_run_data[0][0])
-                    for j in range(len(multi_run_data)):
-                        for z in range(len(multi_run_data[j][i])):
-                            if flag1[z]==-1:
-                                flag1[z]=float(multi_run_data[j][i][z])
-                            else:
-                                flag1[z]=flag1[z]+float(multi_run_data[j][i][z])
-                    for j in range(len(flag1)):
-                        flag1[j]=flag1[j]/len(multi_run_data)
-                        if flag2[j]==-1:
-                            flag2[j]=flag1[j]
+    def dump_sumof(self,algo,bias,data_in,dMR,BASE,PATH,MSG_EXP_TIME,msg_hops):
+        for l in range(len(data_in)):
+            multi_run_data = data_in[l]
+            flag2 = [-1]*len(multi_run_data[0][0])
+            for i in range(len(multi_run_data[0])):
+                flag1 = [-1]*len(multi_run_data[0][0])
+                for j in range(len(multi_run_data)):
+                    for z in range(len(multi_run_data[j][i])):
+                        if flag1[z]==-1:
+                            flag1[z]=float(multi_run_data[j][i][z])
                         else:
-                            flag2[j]=flag1[j]+flag2[j]
-                for i in range(len(flag2)):
-                    flag2[i]=flag2[i]/len(multi_run_data[0])
-                self.dump_resume_csv(algo,l,bias,np.round(flag2,2).tolist(),"-",BASE,PATH,"-","-","-",MSG_EXP_TIME,msg_hops,dMR)
-        
+                            flag1[z]=flag1[z]+float(multi_run_data[j][i][z])
+                for j in range(len(flag1)):
+                    flag1[j]=flag1[j]/len(multi_run_data)
+                    if flag2[j]==-1:
+                        flag2[j]=flag1[j]
+                    else:
+                        flag2[j]=flag1[j]+flag2[j]
+            for i in range(len(flag2)):
+                flag2[i]=flag2[i]/len(multi_run_data[0])
+            self.dump_resume_csv(algo,l,bias,np.round(flag2,2).tolist(),"-",BASE,PATH,"-","-","-",MSG_EXP_TIME,msg_hops,dMR)
+    
 ##########################################################################################################
     def dump_quorum(self,algo,bias,data_in,BASE,PATH,COMMIT,THR,MINS,MSG_EXP_TIME,msg_hops):
         flag2=[-1]*len(data_in[0][0])
