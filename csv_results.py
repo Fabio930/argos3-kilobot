e9b0abfffdf114593a775f67be60108018e1e2ee
Fabio Oddi <fabio.oddi@uniroma1.it>
2025-04-04 16:56:03 +0200

    .

--- csv_results.py
+++ csv_results.py
@@ -63,8 +63,7 @@ class Data:
         fitted_data = {}
         for i in range(len(data_in)):
             for k in data_in[i].keys():
-                print(k)
-                estimates = self.fit_recovery(k[0],k[1],k[4],k[5],k[7],k[8],0,0,data_in[i].get(k)) # find k for rebroadcast and hops
+                estimates = self.fit_recovery(k[0],k[1],k[4],k[5],k[7],k[8],k[3],k[6],data_in[i].get(k)) # find k for rebroadcast and hops
                 for z in estimates.keys():
                     fitted_data.update({(k[0],k[1],k[2],k[3],k[4],k[5],k[6],k[7],k[8],z):estimates.get(z)})
         return fitted_data
