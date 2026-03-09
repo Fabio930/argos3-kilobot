51b9e4e541d931ead4d4abd0860484bcda344aea
Fabio930 <fabioddi24@gmail.com>
2026-03-09 15:57:08 +0100

    .

--- src/examples/behaviors/bestN.c
+++ src/examples/behaviors/bestN.c
@@ -475,7 +475,7 @@ void loop(){
     apply_state_changes();
     check_quorum(&quorum_array);
     if(init_received_C) talk();
-    fprintf(fp,"%d\t%d\t%d\n",my_state,quorum_reached,num_quorum_items);
+    fprintf(fp,"%d\t%d\t%d\n",my_state,quorum_reached,true_quorum_items);
     if(quorum_reached==1 && my_state==committed) set_color(RGB(3,0,0));
     else if(quorum_reached==1 && my_state==uncommitted) set_color(RGB(3,3,0));
     else set_color(led);
