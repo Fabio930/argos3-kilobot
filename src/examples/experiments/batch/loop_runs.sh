2881e1868b7f103369d7d1d1f3058dcaa5369ee7
Fabio930 <fabioddi24@gmail.com>
2026-03-13 18:36:14 +0100

    .

--- src/examples/experiments/batch/loop_runs.sh
+++ src/examples/experiments/batch/loop_runs.sh
@@ -66,18 +66,18 @@ fi
 #######################################
 experiment_length="900"
 RUNS=100
-rebroadcast="1"
-msg_expiring_sec="60 300 600"
-numrobots="25 100"
+rebroadcast="2"
+msg_expiring_sec="60 120 180 300 600"
+numrobots="25"
 threshold="0.80"
 committed_percentage="0.68 0.76 0.84"
 messages_hops="1"
 # small arena dimensions
-# arena_x_side="0.500 1.000"
-# arena_y_side="0.500 0.250"
+arena_x_side="0.500 1.000"
+arena_y_side="0.500 0.250"
 # big arena dimensions
-arena_x_side="1.000 2.000"
-arena_y_side="1.000 0.500"
+# arena_x_side="1.000 2.000"
+# arena_y_side="1.000 0.500"
 
 # Convert the space-separated strings into arrays
 arena_x_side_array=($arena_x_side)
